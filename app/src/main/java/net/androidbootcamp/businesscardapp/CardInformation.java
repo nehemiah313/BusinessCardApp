24cb7ba5bf30a6f64387be20f200d71cdf1cdf05
nehemiah313 <60307321+nehemiah313@users.noreply.github.com>
2022-12-15 13:11:34 +0300

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/businesscardapp/CardInformation.java
@@ -0,0 +1,14 @@
+package net.androidbootcamp.businesscardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CardInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_information);
+    }
+}
